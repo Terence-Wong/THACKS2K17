ef32c5c0fc07f5d8529f11433d5a280f43f99305
Terence-Wong <terencewong.work@gmail.com>
2017-10-21 20:51:37 -0400

    bug fix

--- js/show.js
+++ js/show.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		var tags_request = document.getElementById("search_bar").value;
 		xhr.open('POST', 'http://100.64.95.208:3000/data?tags='+tags_request, true);
 		xhr.send();
+		document.getElementById('data').scrollIntoView({block: 'start', behavior: 'smooth'});
     });  
 	
 	$("#hide_results").click(function(){
