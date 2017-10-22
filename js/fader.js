d087252892fb7c2b525f5966d99ec21257e2a3a8
Terence-Wong <terencewong.work@gmail.com>
2017-10-22 00:44:56 -0400

    formating

--- js/fader.js
+++ js/fader.js
@@ -1,11 +1,15 @@
-var divs = $('#input'),
+var divs = $('#input,#home'),
     limit = 600;  /* scrolltop value when opacity should be 0 */
-
+var hivs = $('#data'),
+    himit = 200;  /* scrolltop value when opacity should be 0 */
 $(window).on('scroll', function() {
    var st = $(this).scrollTop();
 
    /* avoid unnecessary call to jQuery function */
    if (st <= limit) {
       divs.css({ 'opacity' : (1 - st/limit) });
+   }
+   if(himit <= st){
+	  hivs.css({ 'opacity' : (1-(1 - st/limit)) });
    }
 });
