522870e07c77c3b69287f945a97a32f2666ac9c9
Enkrypton <leo.jia@student.tdsb.on.ca>
2017-10-22 06:51:09 -0400

    loading

--- js/show.js
+++ js/show.js
@@ -2,11 +2,13 @@ window.data=[]
 $(document).ready(function(){
     
     $("#show_results").click(function(){
-    		  window.data=[]
-			  CreateTableFromJSON();
+    	window.data=[]
+		CreateTableFromJSON();
 		var xhr = new XMLHttpRequest();
 		var tags_request = document.getElementById("search_bar").value;
 		xhr.open('POST', 'http://100.64.95.208:3000/data?tags='+tags_request, true);
+		var divLoad = document.getElementById("showData")
+		divLoad.innerHTML = "<h1><span class='animate-flicker'>NOW LOADING...</span></h1>";
 		 xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4) {
 			  window.data=JSON.parse(xhr.response); //Outputs a DOMString by default
