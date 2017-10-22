9969dcf5189e78146bad71cf03693bcd9dc5d56e
Enkrypton <leo.jia@student.tdsb.on.ca>
2017-10-21 20:26:27 -0400

    basically everything

--- js/show.js
+++ js/show.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     
     $("#show_results").click(function(){
-        $("#results").fadeIn();
+		var xhr = new XMLHttpRequest();
+		var tags_request = document.getElementById("search_bar").value;
+		xhr.open('POST', 'http://100.64.95.208:3000/data?tags='+tags_request, true);
+		xhr.send();
     });  
 	
 	$("#hide_results").click(function(){
